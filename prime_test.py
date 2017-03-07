829cc6900e6d91d41cca26473075e50fe5f1e119
Christine <christine.apimo27@gmail,com>
2017-03-07 15:59:19 +0300

    test for true is prime

---
+++ prime_test.py
@@ -0,0 +1,10 @@
+import unittest
+from prime.py import prime_number
+
+class prime_test (unittest.Testcase):
+	def test_prime(self):
+		self.assertTrue (test_prime.prime_number(5))
+if __name__=="__main__":
+		unittest.main()
+
+
